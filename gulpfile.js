86416dbbed65918b9af857fae1ba42042580c635
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2023-04-28 14:50:15 +0200

    demo tailwind config added

--- gulpfile.js
+++ gulpfile.js
@@ -1,167 +1,167 @@
 // imports
-import pkg from 'gulp';
-import babel from 'gulp-babel';
-import concat from 'gulp-concat';
-import uglify from 'gulp-uglify';
-import { deleteAsync } from 'del';
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import autoprefixer from 'autoprefixer';
-import sourcemaps from 'gulp-sourcemaps';
-import postcss from 'gulp-postcss';
-import terser from 'gulp-terser';
-import cssnano from 'cssnano';
-import tailwindcss from 'tailwindcss';
-import browserSync from 'browser-sync';
-import pug from 'gulp-pug';
-import rename from 'gulp-rename';
-import through from 'through2';
-import mode from 'gulp-mode';
+import pkg from "gulp";
+import babel from "gulp-babel";
+import concat from "gulp-concat";
+import uglify from "gulp-uglify";
+import { deleteAsync } from "del";
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import autoprefixer from "autoprefixer";
+import sourcemaps from "gulp-sourcemaps";
+import postcss from "gulp-postcss";
+import terser from "gulp-terser";
+import cssnano from "cssnano";
+import tailwindcss from "tailwindcss";
+import browserSync from "browser-sync";
+import pug from "gulp-pug";
+import rename from "gulp-rename";
+import through from "through2";
+import mode from "gulp-mode";
 
 const { dest, lastRun, parallel, series, src, watch } = pkg;
 
 // File path variables etc.
 const gulpMode = mode({
-    modes: ['production', 'development'],
-    default: 'development',
-    verbose: false
+  modes: ["production", "development"],
+  default: "development",
+  verbose: false,
 });
-const dev_url = 'yourlocal.dev';
+const dev_url = "yourlocal.dev";
 const sass = gulpSass(dartSass);
 const files = {
-    scssPath: {
-        src: 'src/scss/**/*.scss',
-        dest: 'dist/css'
-    },
-    jsPath: {
-        src: 'src/js/components/*.js',
-        dest: 'dist/js'
-    },
-    imgPath: {
-        src: 'src/img/**/*.{jpg,jpeg,png,svg}',
-        dest: 'dist/img'
-    },
-    pugPath: {
-        src: 'src/pug/**/*.pug',
-        dest: 'dist'
-    }
+  scssPath: {
+    src: "src/scss/**/*.scss",
+    dest: "dist/css",
+  },
+  jsPath: {
+    src: "src/js/components/*.js",
+    dest: "dist/js",
+  },
+  imgPath: {
+    src: "src/img/**/*.{jpg,jpeg,png,svg}",
+    dest: "dist/img",
+  },
+  pugPath: {
+    src: "src/pug/**/*.pug",
+    dest: "dist",
+  },
 };
 
 // Browsersync to spin up a local server
 const browserSyncServe = (cb) => {
-    // initializes browsersync server
-    browserSync.init({
-        server: {
-            baseDir: 'dist'
-            // proxy: dev_url,
-        },
-        notify: {
-            styles: {
-                top: 'auto',
-                bottom: '0'
-            }
-        }
-    });
-    cb();
+  // initializes browsersync server
+  browserSync.init({
+    server: {
+      baseDir: "dist",
+      // proxy: dev_url,
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0",
+      },
+    },
+  });
+  cb();
 };
 const browserSyncReload = (cb) => {
-    // reloads browsersync server
-    browserSync.reload();
-    cb();
+  // reloads browsersync server
+  browserSync.reload();
+  cb();
 };
 
 // Sass Task
 const scssTask = async () => {
-    return src(files.scssPath.src)
-        .pipe(gulpMode.development(sourcemaps.init()))
-        .pipe(
-            sass({
-                outputStyle: 'compressed',
-                includePaths: ['./node_modules']
-            }).on('error', sass.logError)
-        )
-        .pipe(postcss([autoprefixer(), cssnano(), tailwindcss()]))
-        .pipe(gulpMode.development(sourcemaps.write('.')))
-        .pipe(dest(files.scssPath.dest));
+  return src(files.scssPath.src)
+    .pipe(gulpMode.development(sourcemaps.init()))
+    .pipe(
+      sass({
+        outputStyle: "compressed",
+        includePaths: ["./node_modules"],
+      }).on("error", sass.logError)
+    )
+    .pipe(postcss([autoprefixer(), cssnano(), tailwindcss()]))
+    .pipe(gulpMode.development(sourcemaps.write(".")))
+    .pipe(dest(files.scssPath.dest));
 };
 
 const jsTask = async () => {
-    return src(files.jsPath.src, { since: lastRun(jsTask) })
-        .pipe(
-            babel({
-                presets: ['@babel/preset-env']
-            })
-        )
-        .pipe(concat('scripts.js'))
-        .pipe(dest(files.jsPath.dest))
-        .pipe(rename('scripts.min.js'))
-        .pipe(uglify())
-        .pipe(dest(files.jsPath.dest))
-        .pipe(gulpMode.development(sourcemaps.init({ loadMaps: true })))
-        .pipe(
-            through.obj(function (file, enc, cb) {
-                // Dont pipe through any source map files as it will be handled
-                // by gulp-sourcemaps
-                const isSourceMap = /\.map$/.test(file.path);
-                if (!isSourceMap) this.push(file);
-                cb();
-            })
-        )
-        .pipe(gulpMode.development(sourcemaps.write('.')))
-        .pipe(gulpMode.production(terser()));
+  return src(files.jsPath.src, { since: lastRun(jsTask) })
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(concat("scripts.js"))
+    .pipe(dest(files.jsPath.dest))
+    .pipe(rename("scripts.min.js"))
+    .pipe(uglify())
+    .pipe(dest(files.jsPath.dest))
+    .pipe(gulpMode.development(sourcemaps.init({ loadMaps: true })))
+    .pipe(
+      through.obj(function (file, enc, cb) {
+        // Dont pipe through any source map files as it will be handled
+        // by gulp-sourcemaps
+        const isSourceMap = /\.map$/.test(file.path);
+        if (!isSourceMap) this.push(file);
+        cb();
+      })
+    )
+    .pipe(gulpMode.development(sourcemaps.write(".")))
+    .pipe(gulpMode.production(terser()));
 };
 
 // HTML Task
 const htmlTask = async () => {
-    return src('./src/pug/views/*.pug', { since: lastRun(htmlTask) })
-        .pipe(pug({ pretty: true }))
-        .pipe(dest('./dist'));
+  return src(["./src/pug/views/*.pug", "./src/pug/inludes/*.pug"])
+    .pipe(pug({ pretty: true }))
+    .pipe(dest("./dist"));
 };
 
 // moveWebfontsToDist Task
 const moveWebfontsToDist = async () => {
-    return src(['src/webfonts/**'], {
-        since: lastRun(moveWebfontsToDist)
-    }).pipe(dest('dist/webfonts'));
+  return src(["src/fonts/**"], {
+    since: lastRun(moveWebfontsToDist),
+  }).pipe(dest("dist/fonts"));
 };
 
 // Browsersync Watch task
 // Watch HTML file for change and reload browsersync server
 // watch SCSS and JS files for changes, run scss and js tasks simultaneously and update browsersync
 const bsWatchTask = async () => {
-    watch(
-        [files.scssPath.src, files.jsPath.src, files.pugPath.src],
-        { interval: 1000, usePolling: true }, //Makes docker work
-        series(parallel(scssTask, jsTask, htmlTask), browserSyncReload)
-    );
+  watch(
+    [files.scssPath.src, files.jsPath.src, files.pugPath.src],
+    { interval: 1000, usePolling: true }, //Makes docker work
+    series(parallel(scssTask, jsTask, htmlTask), browserSyncReload)
+  );
 };
 
 // Images Task
 const imagesTask = async () => {
-    return src(files.imgPath.src, { since: lastRun(imagesTask) }).pipe(
-        dest(files.imgPath.dest)
-    );
+  return src(files.imgPath.src, { since: lastRun(imagesTask) }).pipe(
+    dest(files.imgPath.dest)
+  );
 };
 
 // Clean dist task
 const cleanDist = async (done) => {
-    return deleteAsync(['dist/**/*'], done());
+  return deleteAsync(["dist/**/*"], done());
 };
 
 // Dev Task
 export const dev = series(
-    cleanDist,
-    parallel(scssTask, jsTask, htmlTask),
-    parallel(moveWebfontsToDist, imagesTask),
-    browserSyncServe,
-    bsWatchTask
+  cleanDist,
+  parallel(scssTask, jsTask, htmlTask),
+  parallel(moveWebfontsToDist, imagesTask),
+  browserSyncServe,
+  bsWatchTask
 );
 
 // Build Task
 export const build = series(
-    cleanDist,
-    parallel(scssTask, jsTask, htmlTask),
-    parallel(moveWebfontsToDist, imagesTask)
+  cleanDist,
+  parallel(scssTask, jsTask, htmlTask),
+  parallel(moveWebfontsToDist, imagesTask)
 );
 
 export const clean = series(cleanDist);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,67 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-    content: ['./src/**/*.{html,js,pug}'],
-    theme: {
-        container: {
-            center: true,
-            padding: {
-                DEFAULT: '1rem',
-                sm: '2rem',
-                lg: '4rem',
-                xl: '5rem',
-                '2xl': '6rem'
-            }
-        }
+  content: ["./src/**/*.{html,js,pug}"],
+  theme: {
+    container: {
+      center: true,
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('tailwind-scrollbar'),
-        require('@tailwindcss/forms')
-    ]
+    colors: {
+      black: {
+        100: "#d2d2d2",
+        200: "#a5a5a5",
+        300: "#787878",
+        400: "#4b4b4b",
+        500: "#1e1e1e",
+        600: "#181818",
+        700: "#121212",
+        800: "#0c0c0c",
+        900: "#060606",
+      },
+      orange: {
+        100: "#fee4d9",
+        200: "#fec9b3",
+        300: "#fdaf8e",
+        400: "#fd9468",
+        500: "#fc7942",
+        600: "#ca6135",
+        700: "#974928",
+        800: "#65301a",
+        900: "#32180d",
+      },
+      graygreen: {
+        100: "#edf7f0",
+        200: "#dbefe1",
+        300: "#cae8d3",
+        400: "#b8e0c4",
+        500: "#a6d8b5",
+        600: "#85ad91",
+        700: "#64826d",
+        800: "#425648",
+        900: "#212b24",
+      },
+      whitegreen: {
+        100: "#fcfdfa",
+        200: "#f9fcf5",
+        300: "#f7faf1",
+        400: "#f4f9ec",
+        500: "#f1f7e7",
+        600: "#c1c6b9",
+        700: "#91948b",
+        800: "#60635c",
+        900: "#30312e",
+      },
+    },
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
+      catamaran: "Catamaran, sans-serif",
+    },
+    extend: {
+      boxShadow: ({ theme }) => ({
+        custom: `4px 4px 0 ${theme("colors.black.500")}`,
+      }),
+    },
+  },
+  darkMode: ["dark", '[data-mode="dark"]'],
+  plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar")],
 };
